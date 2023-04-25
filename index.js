e3ceb492d87b9bd5cd3f4e50b16581aa782e6239
Bradley <bradleytroll@gmail.com>
2023-04-25 19:26:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  } else if 
+    (someValue <= 2000) {
+      result = 'That will be twenty bucks.';
+    } else if
+    (someValue <= 2499) {
+      result = 'I will gladly take your thirty bucks.'
+    } else if
+    (someValue > 2500) {
+      result = 'No can do.'
+    }
+  
+  return result
+  }
+
+function ternaryCheckCity(city){
+ return city === 'NYC' ? "Ok, sounds good." : "No go."; 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
