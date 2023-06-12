5701b7cd38891b51fac334d3192b59fc10552145
Bradley Troll <bradleytroll@gmail.com>
2023-06-12 15:12:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,35 +1,89 @@
-function scuberGreetingForFeet(rideLength){
-  let result
-  if (rideLength < 400) {
-    result = `This one is on me!`
+//Redoing this lab for extra practice. Original code commented out below.
+
+function scuberGreetingForFeet(distance) {
+  let fare;
+  if (distance <= 400) {
+    fare = 'This one is on me!';
+  } else if (distance <= 2000) {
+    fare = "That will be twenty bucks.";
+  } else if (distance <= 2500) {
+    fare = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    fare = "No can do.";
   }
-  else if (rideLength <= 2000) {
-    result = `That will be twenty bucks.`
-  }
-  else if (rideLength < 2500) {
-    result = `I will gladly take your thirty bucks.`
-  }
-  else if (rideLength >= 2500) {
-    result = `No can do.`
-  }
-  return result
+  return fare;
 }
 
-function ternaryCheckCity(city){
-  return city === 'NYC' ? "Ok, sounds good." : "No go.";
+function ternaryCheckCity(city) {
+  let cityCheck;
+  if (city === 'NYC') {
+    cityCheck = "Ok, sounds good.";
+  } else if (city !== 'NYC') {
+    cityCheck = 'No go.'
+  }
+  return cityCheck;
 }
 
-function switchOnCharmFromTip(tip){
-  switch (tip){
-    case 'generous':
-      return "Thank you so much.";
-      break;
-    case 'not as generous':
-      return "Thank you.";
-      break;
-    default:
-      return 'Bye.';
-}};
+function switchOnCharmFromTip(tip) {
+switch(tip) {
+  case 'generous': return "Thank you so much.";
+  break;
+  case 'not as generous': return "Thank you.";
+  break;
+  default: return "Bye."
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function scuberGreetingForFeet(rideLength){
+//   let result
+//   if (rideLength < 400) {
+//     result = `This one is on me!`
+//   }
+//   else if (rideLength <= 2000) {
+//     result = `That will be twenty bucks.`
+//   }
+//   else if (rideLength < 2500) {
+//     result = `I will gladly take your thirty bucks.`
+//   }
+//   else if (rideLength >= 2500) {
+//     result = `No can do.`
+//   }
+//   return result
+// }
+
+// function ternaryCheckCity(city){
+//   return city === 'NYC' ? "Ok, sounds good." : "No go.";
+// }
+
+// function switchOnCharmFromTip(tip){
+//   switch (tip){
+//     case 'generous':
+//       return "Thank you so much.";
+//       break;
+//     case 'not as generous':
+//       return "Thank you.";
+//       break;
+//     default:
+//       return 'Bye.';
+// }};
  
  
  
